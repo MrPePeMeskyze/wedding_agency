913cb364d4716822e6f9076a86e418d549b1c5f9
Sergey <predatorqwerty2@gmail.com>
2016-09-10 12:12:56 +0300

    no message

---
+++ vendor/bundle/ruby/2.2.0/bin/scss.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"c:\RailsInstaller\Ruby2.2.0\bin\ruby.exe" "c:/Users/Sergey/wedding_agency/vendor/bundle/ruby/2.2.0/bin/scss" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"c:\RailsInstaller\Ruby2.2.0\bin\ruby.exe" "%~dpn0" %*
---
+++ vendor/bundle/ruby/2.2.0/gems/capistrano-2.15.9/lib/capistrano/version.rb
@@ -0,0 +1,13 @@
+module Capistrano
+  class Version
+    MAJOR = 2
+    MINOR = 15
+    PATCH = 9
+
+    def self.to_s
+      "#{MAJOR}.#{MINOR}.#{PATCH}"
+    end
+  end
+
+  VERSION = Version.to_s
+end
