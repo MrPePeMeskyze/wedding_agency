ef393198e854670e028ace3a0fc9b2751900dc22
Sergey <predatorqwerty2@gmail.com>
2016-09-10 11:53:01 +0300

    [-] Адаптив шапки + шрифты + лого + wysiwyg

--- app/assets/javascripts/admin/scripts.js
+++ app/assets/javascripts/admin/scripts.js
@@ -4,5 +4,17 @@ $(document).ready(function() {
 		format: 'DD/MM/YYYY'
 	})
 	autosize(document.querySelectorAll('textarea'));
+	
+	$('.summernote_initial').summernote({
+		toolbar: [
+			['style', ['bold', 'italic', 'underline', 'strikethrough', 'clear']],
+			['fontsize', ['fontsize']],
+			['color', ['color']],
+			['para', ['ul', 'ol', 'paragraph']],
+			['insert', ['picture','link', 'table', 'hr']],
+			['misk', ['undo','redo', 'codeview']]
+		  ]
+	});
+	$('.form-group.note-group-select-from-files').remove();
 });
   
