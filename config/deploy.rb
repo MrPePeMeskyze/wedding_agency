0827c3b807cc1bb44237f43b3fc71530c175ffc3
Sergey <predatorqwerty2@gmail.com>
2016-09-10 12:22:19 +0300

    no message

--- config/deploy.rb
+++ config/deploy.rb
@@ -83,7 +83,7 @@ set :scm,             :git
 #set :repository,      "ssh://#{user}@#{deploy_server}/home/#{user}/git/#{application}.git"
 
 ## Если ваш репозиторий в GitHub, используйте такую конфигурацию
- set :repository,    "git://github.com/MrPePeMeskyze/wedding_agency.git"
+ set :repository,    "git@github.com:MrPePeMeskyze/wedding_agency.git"
 
 ## --- Ниже этого места ничего менять скорее всего не нужно ---
 
