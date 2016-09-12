604b0b9653721161918fd032cf55d39e8fb89bfb
Sergey <predatorqwerty2@gmail.com>
2016-09-12 18:54:17 +0300

    [+] Изменение стилей

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,8 +42,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp  
-  config.action_mailer.default_charset = "utf-8"  
+  config.action_mailer.delivery_method = :smtp   
    ActionMailer::Base.smtp_settings = {  
     :address              => "smtp.mail.ru",  
     :port                 => 465 ,  
