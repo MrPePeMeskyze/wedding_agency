93e945f0ce061fa5f906d88af1db916b08b6dccb
Sergey <predatorqwerty2@gmail.com>
2016-09-15 21:28:24 +0300

    меньше видое в видеозаписях

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -9,7 +9,7 @@ class VideosController < ApplicationController
 	def view
 		@video = Objects.where('objects_type_id = 5').first
 		@album = Albums.find_by(permalink: params[:id], is_published: 1)
-		@videos = @album.videos.where('is_published = 1').order('sort_order ASC, created_at desc').paginate(page: params[:page], per_page: 20)
+		@videos = @album.videos.where('is_published = 1').order('sort_order ASC, created_at desc').paginate(page: params[:page], per_page: 10)
 	end
 	
 end
